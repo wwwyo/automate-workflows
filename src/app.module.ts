689fbcc757e3883fc80317fe4e8d423f55173c38
yuito118 <72675978+yuito118@users.noreply.github.com>
2022-08-15 17:51:53 +0900

    install: env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SlackModule } from './slack/slack.module';
+import { NotionService } from './notion/notion.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [SlackModule, ConfigModule.forRoot({ envFilePath: ['.env'] })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, NotionService],
 })
 export class AppModule {}

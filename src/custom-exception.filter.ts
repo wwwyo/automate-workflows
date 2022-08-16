de7dfde20a23797dbb43962a75873a0725d3e28a
yuito118 <72675978+yuito118@users.noreply.github.com>
2022-08-17 00:47:56 +0900

    add: error handle

---
+++ src/custom-exception.filter.ts
@@ -0,0 +1,29 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { SlackService } from './slack/slack.service';
+
+@Catch()
+export class CustomExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    new SlackService().postMessage(undefined, `error: ${exception['message']}`);
+    const context = host.switchToHttp();
+    const response = context.getResponse<Response>();
+    const request = context.getRequest<Request>();
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CustomExceptionFilter } from './custom-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new CustomExceptionFilter());
   await app.listen(Number(process.env.PORT) || '3000', '0.0.0.0');
 }
 bootstrap();

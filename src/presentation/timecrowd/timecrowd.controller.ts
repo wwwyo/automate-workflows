8435223c4072539ab4ff1ebd2e410196236ccd3d
yuito118 <72675978+yuito118@users.noreply.github.com>
2022-11-27 00:25:25 +0900

    add: stop timecrowd controller action

--- src/presentation/timecrowd/timecrowd.controller.ts
+++ src/presentation/timecrowd/timecrowd.controller.ts
@@ -1,18 +1,32 @@
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
-import StartTimeCrowdInput from 'src/usecase/timecrowd/start-timecrowd.command';
+import StartTimeCrowdCommand from 'src/usecase/timecrowd/start-timecrowd.command';
+import StopTimeCrowdCommand from 'src/usecase/timecrowd/stop-timecrowd.command';
+import StopTimeCrowdUseCase from 'src/usecase/timecrowd/stop-timecrowd.usecase';
 import StartTimeCrowdUseCase from '../../usecase/timecrowd/start-timecrowd.usecase';
-import StartTimeCrowdRequestDto from './handle-timecrowd.request.dto';
+import HandleTimeCrowdRequestDto from './handle-timecrowd.request.dto';
 
 @Controller('timecrowd')
 export default class TimeCrowdController {
-  constructor(private readonly startTimeCrowdUseCase: StartTimeCrowdUseCase) {}
+  constructor(
+    private readonly startTimeCrowdUseCase: StartTimeCrowdUseCase,
+    private readonly stopTimeCrowdUseCase: StopTimeCrowdUseCase,
+  ) {}
 
   @Post('start')
   @HttpCode(201)
   start(
     @Body()
-    request: StartTimeCrowdRequestDto,
+    request: HandleTimeCrowdRequestDto,
   ): void {
-    this.startTimeCrowdUseCase.handle(new StartTimeCrowdInput(request));
+    this.startTimeCrowdUseCase.handle(new StartTimeCrowdCommand(request));
+  }
+
+  @Post('stop')
+  @HttpCode(201)
+  stop(
+    @Body()
+    request: HandleTimeCrowdRequestDto,
+  ): void {
+    this.stopTimeCrowdUseCase.handle(new StopTimeCrowdCommand(request));
   }
 }

5df8c4824c0ecbb260c27d3c0732a280647d6c7d
yuito118 <72675978+yuito118@users.noreply.github.com>
2022-11-26 23:29:35 +0900

    add: app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,14 @@ import { AppService } from './app.service';
 import { SlackModule } from './slack/slack.module';
 import { NotionService } from './notion/notion.service';
 import { ConfigModule } from '@nestjs/config';
-import { TimecrowdModule } from './timecrowd/timecrowd.module';
+import { TimecrowdModule } from './modules/timecrowd.module';
 
 @Module({
-  imports: [SlackModule, ConfigModule.forRoot({ envFilePath: ['.env'] }), TimecrowdModule],
+  imports: [
+    SlackModule,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.env'] }),
+    TimecrowdModule,
+  ],
   controllers: [AppController],
   providers: [AppService, NotionService],
 })

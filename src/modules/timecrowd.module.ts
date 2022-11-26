8bd01c37979af20523acd6a6ea68593a3bf9bbf1
yuito118 <72675978+yuito118@users.noreply.github.com>
2022-11-27 00:25:57 +0900

    update: stop usecaseをmoduleに追加

--- src/modules/timecrowd.module.ts
+++ src/modules/timecrowd.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import SlackClient from 'src/infrastructure/module/slack.client';
 import StartTimeCrowdUseCase from 'src/usecase/timecrowd/start-timecrowd.usecase';
 import TimeCrowdController from 'src/presentation/timecrowd/timecrowd.controller';
+import StopTimeCrowdUseCase from 'src/usecase/timecrowd/stop-timecrowd.usecase';
 
 @Module({
-  providers: [SlackClient, StartTimeCrowdUseCase],
+  providers: [SlackClient, StartTimeCrowdUseCase, StopTimeCrowdUseCase],
   controllers: [TimeCrowdController],
 })
 export class TimecrowdModule {}

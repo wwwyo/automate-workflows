4d27f97a48596852be8868b7f67da2d115d4adb9
yuito118 <72675978+yuito118@users.noreply.github.com>
2022-11-27 21:36:22 +0900

    fix: dayjs format

--- src/domain/datetime.value-object.ts
+++ src/domain/datetime.value-object.ts
@@ -1,19 +1,14 @@
 import * as dayjs from 'dayjs';
-import timezone from 'dayjs/plugin/timezone';
-import utc from 'dayjs/plugin/utc';
 
 export default class Datetime {
   private readonly dayjs: dayjs.Dayjs;
 
   constructor(dt: Date) {
-    dayjs.extend(utc);
-    dayjs.extend(timezone);
-    dayjs.tz.setDefault('Asia/Tokyo');
     this.dayjs = dayjs(dt);
   }
 
   get value() {
-    return this.format('YYYY-MM-DD hh:mm:ss');
+    return this.format('YYYY-MM-DD HH:mm:ss');
   }
 
   format(fmt) {
